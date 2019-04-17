52e0b6443d009b483e1ec12e42bffbb8e5b98586
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-04-16 22:15:26 -0500

    Added test to find default buttons in Controls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -5,7 +5,14 @@ import { render, fireEvent } from 'react-testing-library';
 import Controls from './Controls';
 
 describe('<Controls />', () => {
-  it('should ', () => {
+  it('Should render', () => {
     const { getByText } = render(<Controls />);
+  });
+
+  it('Should have a "Close Gate" & "Lock Gate" button by default', () => {
+    const { getByText } = render(<Controls />);
+
+    getByText(/close gate/i);
+    getByText(/lock gate/i);
   });
 });
