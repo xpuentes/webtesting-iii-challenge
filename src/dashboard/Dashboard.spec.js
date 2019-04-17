726ecf1e1f968235fbe3667f3cae8705d1690177
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-04-16 22:01:56 -0500

    Added snapshot test to Dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,11 +1,18 @@
 // Test away
 import React from 'react';
+import renderer from 'react-test-renderer';
 import { render, fireEvent } from 'react-testing-library';
 
 import Dashboard from './Dashboard';
 
 describe('<Dashboard />', () => {
-  it('should ', () => {
+  it('Should match the snapshot', () => {
+    const tree = renderer.create(<Dashboard />);
+
+    expect(tree.toJSON()).toMatchSnapshot(); 
+  });
+
+  it('Should render', () => {
     const { getByText } = render(<Dashboard />);
   });
 });
